eba6d7396e6b35c39c9567813f0ec5ff5b3e43f6
Rakibul Yeasin <rytotul@gmail.com>
2021-07-05 20:18:52 +0600

    Restructured Codebase

---
+++ sslcommerz_python_api/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+"""
+
+SSLCommerz Python API
+
+"""
+
+# Configurations
+SSLCZ_SESSION_API = 'sslcommerz.com/gwprocess/v4/api.php'
+SSLCZ_VALIDATION_API = 'sslcommerz.com/validator/api/validationserverAPI.php'
